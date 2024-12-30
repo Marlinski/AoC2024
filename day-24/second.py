1fee83b5e575c16d16f226eee6c5d61dadc87ee4
Marlinski <loiseau.lucien@gmail.com>
2024-12-30 17:18:35 +0100

    adventofcode - 2024 - day 24 part 2

--- day-24/second.py
+++ day-24/second.py
@@ -1,34 +1,43 @@
 import re
 from itertools import combinations
 import copy
+import os
+import math
 
-puzzle = "day-24/puzzle"
+puzzle = "day-24/puzzle.modified"
 
 content = open(puzzle, "r").read()
 
 wires = {wire: int(input) for (wire, input) in re.findall(r'(\w+): (\w+)', content)}
 gates = [(op,left,right,output) for (left,op,right,output) in re.findall(r'(\w+) (\w+) (\w+) -> (\w+)', content)]
 
+# set the default value (wire unset) 
+for (op,left,right,output) in gates:
+    if output not in wires:
+        wires[output] = -1
+
+
+
+# 2nd TRY: Generates a graph and realized that it was merely an adder:
+# https://en.wikipedia.org/wiki/Adder_(electronics)
+# this gave me an idea...
+# vvvvv XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX vvvvv 
 
 def generate_adder_graph(gates):
      with open("day-24/gates.dot", "w") as f:
         f.write("digraph AdderGraph {\n")
         f.write("  rankdir=LR;\n")  # Left-to-right layout
 
-        for left, op, right, output in gates:
-            f.write(f'  "{left}" -> "{output}" [label="{op}"];\n')
-            f.write(f'  "{right}" -> "{output}" [label="{op}"];\n')
+        for op, left, right, output in gates:
+            f.write(f'  "{output}" [shape=diamond, label="{op}"];\n')
+            f.write(f'  "{left}" -> "{output}" [label={left}]\n')
+            f.write(f'  "{right}" -> "{output}" [label={right}]\n')
+            if output[0] == "z":
+                f.write(f'  "z{output}" [shape=box, style=filled, label={output}];\n')
+                f.write(f'  "{output}" -> "z{output}"\n')
         f.write("}\n")
 
 graph = generate_adder_graph(gates)
-
-# I tried to bruteforce but this approach did not work
-# vvvvv XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX vvvvv 
-
-# se the default value (wire unset) 
-for (op,left,right,output) in gates:
-    if output not in wires:
-        wires[output] = -1
 
 # simulate until all z wire are set
 def sim(wires,gates):
@@ -48,6 +57,79 @@ def sim(wires,gates):
     return wires
 
 
+# print z_num
+def num(wires,register):
+    z_wire = [(zk,zv) for (zk,zv) in wires.items() if zk.startswith(register)]
+    z_ordered = sorted(z_wire, reverse=True)
+    z_str = "".join([str(wv) for (_,wv) in z_ordered])
+    print(z_str)
+    return int(z_str, 2)
+
+def set(wires,register,value):
+    z_wires = {key: value for key,value in wires.items() if key[0] == register}
+    bin_repr = list(bin(value)[2:].zfill(len(z_wires)))[::-1]
+    for i, key in enumerate(sorted(z_wires.keys())):
+        wires[key] = int(bin_repr[i]) if i < len(bin_repr) else 0
+    return wires
+
+
+# 3rd TRY: let's add two numbers and see which bit starts diverging
+# I am adding 2^50 - 1 to only have 1s on X and adding 1 with y, I should normally get only 0s (except for MSB)
+
+wires = set(wires, "x", int(math.pow(2,45)-1))
+wires = set(wires, "y", 1)
+wires = sim(wires, gates)
+x,y,z = num(wires,"x"), num(wires,"y"), num(wires,"z")
+
+#   111111111111111111111111111111111111111111111
+#   000000000000000000000000000000000000000000001
+#  0111111100000000000000000000000111100000000000
+#                                    ^
+#                                    z11
+# >> diverging at z11 ! 
+# indeed, looking at the graphviz the AND and XOR was swapped so I swapped rpv with z11
+#
+# I swapped them then run again and got this:
+#   111111111111111111111111111111111111111111111
+#   000000000000000000000000000000000000000000001
+#  0111111100000000000000000000001000000000000000
+#                                ^
+# >> diverging at z15 ! 
+# indeed, looking at the graphviz this time it was the internal gates rpb and ctg that was swapped
+#
+# I swapped then and run again:
+#   111111111111111111111111111111111111111111111
+#   000000000000000000000000000000000000000000001
+#  0111111100000000000000000000000000000000000000
+#         ^
+# >> diverging at z38 ! 
+# indeed, looking at the graphviz this time it was the internal gates dvq and z38 that was swapped
+#
+# I swapped then and run again:
+#   111111111111111111111111111111111111111111111
+#   000000000000000000000000000000000000000000001
+#  1000000000000000000000000000000000000000000000
+#
+# looks okay to me! since I only have 3 pairs there must be another combination that somehow makes it ok.
+# Since I was now used to what an adder should be expected to work, I decided to just look through all 
+# the adder and see if anything sticked out, hopefully I quickly found out z31 and dmh was swapped!
+
+
+# I just assembled the solution by hand at this point:
+swapped = ["rpv","z11",   "ctg","rpb",    "dvq","z38",    "dmh","z31"]
+print(",".join(sorted(swapped)))
+
+
+
+
+
+
+
+os._exit(1)
+
+# 1st TRY: I tried to bruteforce but this approach did not work
+# vvvvv XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX vvvvv 
+
 def swap(gates, swapped):
     for (x,y) in swapped:
         (xop,xleft,xright,xoutput) = gates[x]
@@ -55,16 +137,6 @@ def swap(gates, swapped):
         gates[x] = (xop,xleft,xright,youtput)
         gates[y] = (yop,yleft,yright,xoutput)
     return gates
-
-
-# print z_num
-def num(wires,register):
-    z_wire = [(zk,zv) for (zk,zv) in wires.items() if zk.startswith(register)]
-    z_ordered = sorted(z_wire, key=lambda item: item[0], reverse=True)
-    z_str = "".join([str(wv) for (_,wv) in z_ordered])
-    print(z_str)
-    return int(z_str, 2)
-
 
 pairs = combinations(range(len(gates)), 2)
 squads = combinations(pairs, 4)
